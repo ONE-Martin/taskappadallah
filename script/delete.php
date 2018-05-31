2e22e2167171faa6ef011892bd33feaf3f941076
Rémi Pavan <remi.pavan@insa-cvl.fr>
2018-05-31 08:58:13 +0200

    Ajout de la fonctionnalité delete. Pas encore fonctionnel

---
+++ script/delete.php
@@ -0,0 +1,20 @@
+<?php
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+if (isset($data['task'])) {
+    require __DIR__ . '/library.php';
+
+    $task_id = (isset($data['task']['id']) ? $data['task']['id'] : null);
+
+    if ($task_id == null) {
+        http_response_code(401);
+        echo json_encode(['errors' => ["Id  required"]]);
+    }else{
+        // Update the Task
+        $task = new Task();
+        $task->delete($task_id);
+    }
+}
+
+?>
